28f791b7165b96b73b24ab434cd82bd17367e480
SakamotoHiroya <88700870+SakamotoHiroya@users.noreply.github.com>
2026-04-21 01:07:44 +0900

    create template

---
+++ api/cmd/server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "ok")
+	})
+
+	http.ListenAndServe(":"+port, nil)
+}
---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module github.com/SakamotoHiroya/go-cloudrun-todo
+
+go 1.25.5
